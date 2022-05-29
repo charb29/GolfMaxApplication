3911eeab64a4b6703bb18419de5fcaa39cd71ef7
charb29 <ocharb@mac.com>
2022-05-29 14:34:22 -0700

    GET request to get all scores

--- app/src/main/java/com/example/golfmax/UserService.java
+++ app/src/main/java/com/example/golfmax/UserService.java
@@ -1,5 +1,7 @@
 package com.example.golfmax;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
@@ -15,5 +17,8 @@ public interface UserService {
 
     @POST("/scores")
     Call<ScoreResponse> addScore(@Body ScoreRequest scoreRequest);
+
+    @GET("/scores")
+    Call<List<Scores>> getScores();
 
 }
