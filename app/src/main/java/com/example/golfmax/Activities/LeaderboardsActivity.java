2d09fc759195c4b36456822573c4ebdd48c98da6
charb29 <ocharb@mac.com>
2022-06-09 22:52:58 -0700

    leaderboards for all courses

---
+++ app/src/main/java/com/example/golfmax/Activities/LeaderboardsActivity.java
@@ -0,0 +1,92 @@
+package com.example.golfmax.Activities;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import com.example.golfmax.Backend.ApiClient;
+import com.example.golfmax.Backend.DBHelper;
+import com.example.golfmax.Models.Course;
+import com.example.golfmax.Models.Score;
+import com.example.golfmax.RecyclerViews.LeaderboardRV;
+import com.example.golfmax.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class LeaderboardsActivity extends AppCompatActivity {
+
+    public DrawerLayout drawerLayout;
+    public ActionBarDrawerToggle actionBarDrawerToggle;
+    ActionBar actionBar;
+    ColorDrawable colorDrawable;
+    LeaderboardRV leaderboardRV;
+    RecyclerView courseNameRv;
+    List<Course> courseList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_leaderboards);
+        actionBar = getSupportActionBar();
+        colorDrawable = new ColorDrawable(Color.parseColor("#013220"));
+        actionBar.setBackgroundDrawable(colorDrawable);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setTitle("Leaderboards");
+
+        drawerLayout = findViewById(R.id.drawerLayout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.navOpen, R.string.navClose);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        courseList = new ArrayList<>();
+        courseNameRv = (RecyclerView) findViewById(R.id.recyclerViewLeaderboard);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        courseNameRv.setLayoutManager(layoutManager);
+
+        ApiClient.getUserService().getCourseNames().enqueue(new Callback<List<Course>>() {
+            @Override
+            public void onResponse(Call<List<Course>> call, Response<List<Course>> response) {
+                courseList = response.body();
+                Log.i("TAG ====> ", courseList.toString());
+                leaderboardRV = new LeaderboardRV(getApplicationContext(), courseList);
+                courseNameRv.setAdapter(leaderboardRV);
+            }
+
+            @Override
+            public void onFailure(Call<List<Course>> call, Throwable t) {
+                Log.e("FAILED ====> ", t.toString());
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(menuItem)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
+    }
+}
