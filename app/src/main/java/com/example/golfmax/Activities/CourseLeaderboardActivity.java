d4d6018db6ff67b8443ee873360af92ae7e8f5fb
charb29 <ocharb@mac.com>
2022-06-14 18:51:48 -0400

    Leaderboard for course

--- app/src/main/java/com/example/golfmax/Activities/CourseLeaderboardActivity.java
+++ app/src/main/java/com/example/golfmax/Activities/CourseLeaderboardActivity.java
@@ -1,16 +1,35 @@
 package com.example.golfmax.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.TextView;
 
+import com.example.golfmax.Backend.ApiClient;
+import com.example.golfmax.Backend.DBHelper;
+import com.example.golfmax.Models.Course;
+import com.example.golfmax.Models.Score;
 import com.example.golfmax.R;
+import com.example.golfmax.RecyclerViews.CourseLeaderboardRV;
+import com.example.golfmax.RecyclerViews.CourseListRV;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class CourseLeaderboardActivity extends AppCompatActivity {
 
@@ -18,6 +37,13 @@ public class CourseLeaderboardActivity extends AppCompatActivity {
     public ActionBarDrawerToggle actionBarDrawerToggle;
     ActionBar actionBar;
     ColorDrawable colorDrawable;
+    CourseLeaderboardRV courseLeaderboardRV;
+    RecyclerView recyclerView;
+    List<Score> scoreList;
+    TextView textViewCourseName;
+    DBHelper db;
+    Course course;
+    long courseId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +60,40 @@ public class CourseLeaderboardActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        textViewCourseName = findViewById(R.id.textViewCourseName);
+        textViewCourseName.setText(CourseListRV.courseNameForTextView);
+
+        scoreList = new ArrayList<>();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerViewCourseLeaderboard);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        db = new DBHelper(this);
+        course = new Course();
+        course.setId(db.getCourseId(CourseListRV.courseNameForTextView));
+        courseId = course.getId();
+
+        ApiClient.getUserService().getScoresByCourseId(courseId).enqueue(new Callback<List<Score>>() {
+            @Override
+            public void onResponse(Call<List<Score>> call, Response<List<Score>> response) {
+                scoreList = response.body();
+                Log.i("TAG ====> ", scoreList.toString());
+                courseLeaderboardRV = new CourseLeaderboardRV(getApplicationContext(), scoreList);
+                recyclerView.setAdapter(courseLeaderboardRV);
+            }
+
+            @Override
+            public void onFailure(Call<List<Score>> call, Throwable t) {
+                Log.e("FAILED ====> ", t.toString());
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(menuItem)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(menuItem);
     }
 }
---
+++ app/src/main/java/com/example/golfmax/RecyclerViews/CourseLeaderboardRV.java
@@ -0,0 +1,59 @@
+package com.example.golfmax.RecyclerViews;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.golfmax.Models.Score;
+import com.example.golfmax.R;
+
+import java.util.List;
+
+public class CourseLeaderboardRV extends RecyclerView.Adapter<CourseLeaderboardRV.MyViewHolder> {
+
+    private Context context;
+    private List<Score> scoreList;
+
+    public CourseLeaderboardRV(Context context, List<Score> scoreList) {
+        this.context = context;
+        this.scoreList = scoreList;
+    }
+
+    @NonNull
+    @Override
+    public CourseLeaderboardRV.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.course_leaderboard_rv_item, parent, false);
+        return new CourseLeaderboardRV.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CourseLeaderboardRV.MyViewHolder holder, int position) {
+        holder.textViewUsername.setText(scoreList.get(position).getUser().getUsername());
+        holder.textViewUserScore.setText(scoreList.get(position).getUserScore());
+
+        for (int i = 0; i < getItemCount(); i++) {
+            holder.textViewRank.setText(String.valueOf(position + 1));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return scoreList.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textViewUsername, textViewUserScore, textViewRank;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            textViewUsername = (TextView) itemView.findViewById(R.id.textViewUsername);
+            textViewUserScore = (TextView) itemView.findViewById(R.id.textViewUserScore);
+            textViewRank = (TextView) itemView.findViewById(R.id.textViewRank);
+        }
+    }
+}
