a82657c03602be05797c244bbb2f011bd441ed72
charb29 <ocharb@mac.com>
2022-06-23 21:05:14 -0700

    added Api call

--- app/src/main/java/com/example/golfmax/Activities/SettingsActivity.java
+++ app/src/main/java/com/example/golfmax/Activities/SettingsActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,9 +20,8 @@ import com.example.golfmax.Backend.ApiClient;
 import com.example.golfmax.Backend.DBHelper;
 import com.example.golfmax.Models.User;
 import com.example.golfmax.R;
+import com.example.golfmax.RecyclerViews.ScoresRV;
 import com.google.android.material.navigation.NavigationView;
-
-import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -34,9 +34,10 @@ public class SettingsActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
     NavigationView navigationView;
-    TextView textViewUsername, textViewChangeEmail, textViewChangePassword, textViewTotalRounds;
-    DBHelper db;
+    ImageButton imageBtnEditUsername, imageBtnEditEmail, imageBtnEditPassword;
+    TextView textViewUsername, textViewEmail, textViewRoundsPlayed;
     User user;
+    DBHelper db;
     long userId;
 
     @Override
@@ -84,30 +85,34 @@ public class SettingsActivity extends AppCompatActivity {
             }
         });
 
+        imageBtnEditUsername = findViewById(R.id.imageBtnEditUsername);
+        imageBtnEditEmail = findViewById(R.id.imageBtnEditEmail);
+        imageBtnEditPassword = findViewById(R.id.imageBtnEditUsername);
+
+        textViewUsername = findViewById(R.id.textViewUsername);
+        textViewEmail = findViewById(R.id.textViewEmail);
+        textViewRoundsPlayed = findViewById(R.id.textViewRoundsPlayed);
+        textViewRoundsPlayed.setText(Integer.toString(ScoresRV.roundsPlayed));
+
         db = new DBHelper(this);
         user = new User();
-        textViewUsername = findViewById(R.id.textViewUsername);
-        textViewChangeEmail = findViewById(R.id.textViewChangeEmail);
-        textViewChangePassword = findViewById(R.id.textViewChangePassword);
-        textViewTotalRounds = findViewById(R.id.textViewTotalRounds);
         user.setId(db.getUserId(LoginActivity.username));
         userId = user.getId();
 
         ApiClient.getUserService().getUserInfoById(userId).enqueue(new Callback<User>() {
             @Override
             public void onResponse(Call<User> call, Response<User> response) {
-                User userInfo = response.body();
-                textViewUsername.setText(userInfo.getUsername());
-                textViewChangeEmail.setText(userInfo.getEmail());
-                textViewChangePassword.setText(userInfo.getPassword());
+                user = response.body();
+                textViewUsername.setText(user.getUsername());
+                textViewEmail.setText(user.getEmail());
+                Log.i("TAG ====> ", response.body().toString());
             }
 
             @Override
             public void onFailure(Call<User> call, Throwable t) {
-
+                Log.e("FAILED ====> ", t.toString());
             }
         });
-
     }
 
     @Override
