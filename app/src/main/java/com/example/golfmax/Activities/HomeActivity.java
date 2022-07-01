eda12e5615913e7ec4ed23699041db36bccc2c5b
charb29 <ocharb@mac.com>
2022-06-30 21:02:26 -0700

    Displaying average score

--- app/src/main/java/com/example/golfmax/Activities/HomeActivity.java
+++ app/src/main/java/com/example/golfmax/Activities/HomeActivity.java
@@ -10,14 +10,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.golfmax.Backend.ApiClient;
+import com.example.golfmax.Backend.DBHelper;
+import com.example.golfmax.Models.PlayerStatistics;
+import com.example.golfmax.Models.User;
 import com.example.golfmax.Responses.LoginResponse;
 import com.example.golfmax.R;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeActivity extends AppCompatActivity {
 
     CardView cvMyScores, cvPlayRound, cvLeaderboard, cvUserProfile;
+    TextView tvHandicapIndex, tvAverageScore;
     LoginResponse loginResponse;
+    PlayerStatistics statistics;
+    DBHelper db;
+    User user;
+    long userId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +41,27 @@ public class HomeActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
 
-        cvMyScores = findViewById(R.id.card_view_my_scores);
-        cvPlayRound = findViewById(R.id.card_view_play_round);
-        cvLeaderboard = findViewById(R.id.card_view_leaderboards);
-        cvUserProfile = findViewById(R.id.card_view_user_profile);
-
         Intent intent = getIntent();
         if (intent.getExtras() != null) {
             loginResponse = (LoginResponse) intent.getSerializableExtra("data");
             Log.i("LoginActivity", "====> " + loginResponse.getEmail());
             Log.i("LoginActivity", "====> " + loginResponse.getId());
         }
+
+        cvMyScores = findViewById(R.id.card_view_my_scores);
+        cvPlayRound = findViewById(R.id.card_view_play_round);
+        cvLeaderboard = findViewById(R.id.card_view_leaderboards);
+        cvUserProfile = findViewById(R.id.card_view_user_profile);
+
+        tvHandicapIndex = findViewById(R.id.text_view_handicap_index);
+        tvAverageScore = findViewById(R.id.text_view_average_score);
+
+        db = new DBHelper(this);
+        user = new User();
+        statistics = new PlayerStatistics();
+
+        user.setId(db.getUserId(LoginActivity.username));
+        userId = user.getId();
 
         cvMyScores.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +92,20 @@ public class HomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(HomeActivity.this, PlayRoundActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        ApiClient.getUserService().getStatsByUserId(userId).enqueue(new Callback<PlayerStatistics>() {
+            @Override
+            public void onResponse(Call<PlayerStatistics> call, Response<PlayerStatistics> response) {
+                statistics = response.body();
+                tvAverageScore.setText(String.valueOf(statistics.getAverageScore()));
+                Log.i("INFO ====> ", response.toString());
+            }
+
+            @Override
+            public void onFailure(Call<PlayerStatistics> call, Throwable t) {
+                Log.e("ERROR ====> ", t.toString());
             }
         });
     }
