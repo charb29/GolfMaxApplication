9e94b943cfff17f7dd49378adc7e12288cd5a17c
charb29 <ocharb@mac.com>
2022-07-17 13:33:55 -0700

    Displaying user stats

--- app/src/main/java/com/example/golfmaxfinal/activities/HomeActivity.java
+++ app/src/main/java/com/example/golfmaxfinal/activities/HomeActivity.java
@@ -1,10 +1,13 @@
 package com.example.golfmaxfinal.activities;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -14,6 +17,7 @@ import com.example.golfmaxfinal.backend.DBHelper;
 import com.example.golfmaxfinal.contracts.PlayerStatisticsContract;
 import com.example.golfmaxfinal.databinding.ActivityHomeBinding;
 import com.example.golfmaxfinal.models.PlayerStatistics;
+import com.example.golfmaxfinal.models.User;
 import com.example.golfmaxfinal.presenters.PlayerStatisticsPresenter;
 
 import retrofit2.Call;
@@ -22,6 +26,7 @@ import retrofit2.Response;
 
 public class HomeActivity extends Activity implements PlayerStatisticsContract.View {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,34 +34,63 @@ public class HomeActivity extends Activity implements PlayerStatisticsContract.V
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         ActivityHomeBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_home);
-        PlayerStatisticsPresenter presenter = new PlayerStatisticsPresenter(this, getApplicationContext());
-        PlayerStatistics playerStatistics = new PlayerStatistics();
+        long userId = getUserIdByUsername(LoginActivity.username);
+
+        binding.setStats(getPlayerStatsByUserId(userId));
     }
 
     @Override
     public void showData(PlayerStatistics playerStatistics) {}
 
-    private long getUserIdByUsername(String username) {
+    public long getUserIdByUsername(String username) {
         DBHelper db = new DBHelper(this);
-        long userId = db.getUserId(username);
+        User user = new User();
+        user.setId(db.getUserId(username));
+        long userId = user.getId();
 
         return userId;
     }
 
-    private void getPlayerStatsByUserId(long userId) {
-        ApiClient.getApiInterface().getStatsByUserId(userId).enqueue(new Callback<PlayerStatistics>() {
+    @NonNull
+    private PlayerStatistics getPlayerStatsByUserId(long userId) {
+        Call<PlayerStatistics> playerStatisticsCall = ApiClient.getApiInterface().getStatsByUserId(userId);
+        PlayerStatistics playerStatistics = new PlayerStatistics();
+
+        playerStatisticsCall.enqueue(new Callback<PlayerStatistics>() {
             @Override
             public void onResponse(Call<PlayerStatistics> call, Response<PlayerStatistics> response) {
-                PlayerStatistics statistics = response.body();
-                statistics.setHandicapIndex(statistics.getHandicapIndex());
-                statistics.setAverageScore(statistics.getAverageScore());
-                Log.i("PLAYER STATS > ",  statistics.toString());
+                playerStatistics.setAverageScore(response.body().getAverageScore());
+                playerStatistics.setHandicapIndex(response.body().getHandicapIndex());
+
+                Log.i("PLAYER STATS API CALL >", playerStatistics.toString());
             }
 
             @Override
             public void onFailure(Call<PlayerStatistics> call, Throwable t) {
-                Log.e("PLAYERS STATS ERROR > ", t.toString());
+                Log.e("ERROR > ", t.toString());
             }
         });
+
+        return playerStatistics;
+    }
+
+    public void goToPersonalScoresActivity(View view) {
+        Intent intent = new Intent(HomeActivity.this, PersonalScoresActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToNewRoundActivity(View view) {
+        Intent intent = new Intent(HomeActivity.this, NewRoundActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToCourseLeaderboardsActivity(View view) {
+        Intent intent = new Intent(HomeActivity.this, CourseLeaderboardActivity.class);
+        startActivity(intent);
+    }
+
+    public void goToUserProfileActivity(View view) {
+        Intent intent = new Intent(HomeActivity.this, UserProfileActivity.class);
+        startActivity(intent);
     }
 }
