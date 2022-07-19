a1709682619a789c2d997ed908963e02c10435b9
charb29 <ocharb@mac.com>
2022-07-18 19:20:12 -0700

    Removed unused imports

--- app/src/main/java/com/example/golfmaxfinal/backend/ApiInterface.java
+++ app/src/main/java/com/example/golfmaxfinal/backend/ApiInterface.java
@@ -1,6 +1,8 @@
 package com.example.golfmaxfinal.backend;
 
+
 import com.example.golfmaxfinal.models.PlayerStatistics;
+import com.example.golfmaxfinal.models.User;
 import com.example.golfmaxfinal.requests.LoginRequest;
 import com.example.golfmaxfinal.requests.RegistrationRequest;
 import com.example.golfmaxfinal.responses.LoginResponse;
@@ -10,6 +12,7 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 
 public interface ApiInterface {
@@ -22,4 +25,10 @@ public interface ApiInterface {
 
     @GET("stats/user/{id}")
     Call<PlayerStatistics> getStatsByUserId(@Path("id") long id);
+
+    @GET("users/{id}")
+    Call<User> getUserInfoById(@Path("id") long id);
+
+    @PUT("users/{id}/update")
+    Call<User> updateUserInfo(@Path("id") long id, @Body User user);
 }
