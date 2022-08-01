9f62acfb11c53840c64ebdbd9acc3c4ed194d13a
charb29 <ocharb@mac.com>
2022-07-31 19:26:35 -0700

    Empty activity and layout for future use

---
+++ app/src/main/java/com/example/golfmax/activities/ScoreCardBack9Activity.java
@@ -0,0 +1,16 @@
+package com.example.golfmax.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.golfmax.R;
+
+public class ScoreCardBack9Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_score_card_back9);
+    }
+}
